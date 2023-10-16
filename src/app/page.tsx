7cd16d226c7a04e35c483013dc491fefc1726134
Geeth Gunnampalli <geeth.gunnampalli@gmail.com>
2023-10-16 15:18:04 -0500

    feat: componentize home for setup

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,18 +1,11 @@
 import Link from "next/link";
 import React from "react";
+import HomePlaceholder from "../../components/Home/HomePlaceholder";
 
 function Home() {
   return (
     <div className="flex h-screen flex-col items-center justify-center">
-      <h1 className="text-primaryDark text-4xl font-bold md:text-6xl">
-        ACM Chapter at UTD
-      </h1>
-      <Link
-        href="https://portal.acmutd.co"
-        className="text-primary bg-acm-gradient mt-6 flex h-12 w-80 items-center justify-center py-2 font-bold"
-      >
-        Join Us
-      </Link>
+      <HomePlaceholder />
     </div>
   );
 }
