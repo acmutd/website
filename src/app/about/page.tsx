6e3afcc873c5fe3d93ccdeb0c1ebfdc6474348fb
Bhavesh Mandalapu <mandalapubhavesh@gmail.com>
2023-10-16 20:23:03 -0500

    Styling change for description

--- src/app/about/page.tsx
+++ src/app/about/page.tsx
@@ -9,11 +9,11 @@ export default function page() {
     <div className="mx-7 my-4 text-center">
       <div className="flex flex-row items-center gap-x-4">
         <img className="h-[35rem] w-auto" src="/assets/about/about.png" alt="Your Company" />
-        <div className="h-fit w-full text-center align-middle">
+        <div className="h-fit w-full text-center align-middle items-center flex flex-col">
           <div className="text-5xl font-semibold text-primary">
             we&apos;re just a group of people
           </div>
-          <div className="relative mt-4 rounded-3xl bg-opacity-10 bg-gradient-to-br p-8 text-primary backdrop-blur-md">
+          <div className="relative mt-4 rounded-3xl bg-opacity-10 bg-gradient-to-br p-8 text-primary backdrop-blur-md max-w-4xl">
             <div className="absolute left-0 top-0 h-full w-full rounded-3xl border-l-2 border-t-2 border-gray-300 opacity-50"></div>
             <div>{pageDescription}</div>
           </div>
