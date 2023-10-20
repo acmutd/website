d7b6368aab087eaf9585e564e421a25638693ccd
Geeth Gunnampalli <geeth.gunnampalli@gmail.com>
2023-10-19 22:48:47 -0500

    feat: update apply header image with peechi

--- src/components/Apply/ApplyHeader.tsx
+++ src/components/Apply/ApplyHeader.tsx
@@ -3,9 +3,9 @@ import React from 'react';
 
 function ApplyHeader() {
   return (
-    <div className="flex h-[70%] w-full justify-end pl-10 lg:h-[55%]">
+    <div className="flex h-[70%] w-full justify-end pl-10 md:h-[55%]">
       <div className="relative mb-6 w-full overflow-hidden rounded-bl-3xl rounded-tl-3xl border border-primary/50 bg-apply-gradient shadow-2xl">
-        <div className="absolute left-0 top-0 flex h-full w-full flex-col items-center justify-center p-6 lg:flex-row lg:space-x-10">
+        <div className="absolute left-0 top-0 flex h-full w-full flex-col items-center justify-center p-6 md:flex-row md:space-x-10">
           <div className="w-full max-w-md lg:max-w-xl">
             <h1 className="mb-10 text-3xl font-bold text-primary xl:text-5xl">apply</h1>
             <p className="text-lg font-medium text-primaryDark xl:text-2xl">
@@ -18,11 +18,11 @@ function ApplyHeader() {
             </p>
           </div>
           <Image
-            src="/assets/apply/apply-header-image.png"
+            src="/assets/peechi.png"
             width={500}
             height={500}
             alt="apply"
-            className="w-full max-w-md object-cover lg:max-w-xl"
+            className="w-full max-w-xs lg:max-w-lg object-cover"
           />
         </div>
       </div>
