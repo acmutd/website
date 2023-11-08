685e33fbcadfc3ea43aa69ac66dc1abc31c30d73
Ruben Olano <rubendavid2711@gmail.com>
2023-11-07 23:51:39 -0600

    feat: create officer page

---
+++ src/components/Officers/OfficerHeader.tsx
@@ -0,0 +1,16 @@
+const OfficerHeader = () => {
+  return (
+    <div className="flex justify-between px-[11.5rem] pt-20">
+      <div className="text-[#cacaca]">
+        <h1 className="text-[2.5rem] font-bold">meet the team</h1>
+        <p className="w-[42rem] pt-8 text-[1.5rem]">
+          8 divisions. One goal.
+          <br /> Here are the students keeping ACM in motion.
+        </p>
+      </div>
+      <div className="h-[17rem] w-[17rem] rounded-full bg-gradient-to-br from-[#F4A3CB] from-10% to-[#dfc84f2b] to-90%"></div>
+    </div>
+  );
+};
+
+export default OfficerHeader;
