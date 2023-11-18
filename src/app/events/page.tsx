68c0a0826042cab3af419aaed22ff48adcdcebb0
Ruben Olano <rubendavid2711@gmail.com>
2023-11-18 13:55:47 -0600

    feat: create events page

---
+++ src/app/events/page.tsx
@@ -0,0 +1,11 @@
+export default function Events() {
+  return (
+    <div className="h-screen w-full bg-[url(/assets/apply/apply-bg.png)] bg-cover bg-center py-20">
+      <div className="mx-20 flex justify-between pt-16">
+        <div className="flex flex-col">
+          <h1 className="text-4xl text-white">Upcoming Events</h1>
+        </div>
+      </div>
+    </div>
+  );
+}
