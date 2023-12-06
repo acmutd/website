01ef890c8fb870a131631152200e08c6cb39a66d
Ruben Olano <rubendavid2711@gmail.com>
2023-12-06 15:27:22 -0600

    feat: Add ProjOfficers component

--- src/app/projects/page.tsx
+++ src/app/projects/page.tsx
@@ -1,11 +1,15 @@
+import ProjOfficers from '@/components/Projects/ProjOfficers';
 import { ProjHeader } from './../../components/Projects/ProjHeader';
 import ProjCarousel from '@/components/Projects/ProjCarousel';
 
 export default function Page() {
   return (
-    <div className="h-screen w-full bg-[url(/assets/projects/projects-bg.png)] bg-cover bg-center py-20">
+    <div className="h-screen w-full bg-[url(/assets/projects/proj-bg.png)] bg-cover bg-center py-20">
       <ProjCarousel />
-      <ProjHeader />
+      <div className="pl-28">
+        <ProjHeader />
+        <ProjOfficers />
+      </div>
     </div>
   );
 }
--- src/components/Projects/ProjHeader.tsx
+++ src/components/Projects/ProjHeader.tsx
@@ -1,7 +1,7 @@
 import Image from 'next/image';
 export function ProjHeader() {
   return (
-    <div className="pl-28 pt-40 font-sans">
+    <div className="pt-40 font-sans">
       <Image src="/assets/about/projects/projects.png" alt="ACM Projects" width={272} height={80} />
       <p className="w-[55rem] pt-5 text-2xl text-[#CACACA]">
         ACM Projects is a semester-long guided projects initiative for those new to software
