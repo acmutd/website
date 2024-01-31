0e5c5feea64bbf29591b52864994060c1f2eaed3
Ruben Olano <rubendavid2711@gmail.com>
2024-01-30 18:36:02 -0600

    fix: Change .with() call to alternate array

--- src/components/Divisions/Shared/Navigator.tsx
+++ src/components/Divisions/Shared/Navigator.tsx
@@ -2,14 +2,14 @@ import Link from 'next/link';
 import { Division } from './divisionUtil';
 
 const links = ['directors', 'experience', 'workshops', 'faq'];
+const altLinks = ['directors', 'experience', 'winning projects', 'faq'];
 
 type Props = {
   division: Division;
 };
 
 export default function Navigator({ division }: Props) {
-  const routeLinks =
-    division === 'research' || division === 'projects' ? links.with(2, 'winning projects') : links;
+  const routeLinks = division === 'research' || division === 'projects' ? altLinks : links;
   return (
     <div className="sticky right-5 top-1/3 z-50 flex h-full w-full flex-col space-y-5 pr-10 text-right">
       {routeLinks.map((link) => (
