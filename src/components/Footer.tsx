45201d6516504afc0e968d8e2a1ce7303d89c166
Bhavesh Mandalapu <mandalapubhavesh@gmail.com>
2024-02-11 13:09:55 -0600

    Feat: Footer

---
+++ src/components/Footer.tsx
@@ -0,0 +1,54 @@
+import Image from 'next/image';
+import { footerData } from '../../config/footer.config';
+import Link from 'next/link';
+
+export default function Footer() {
+  const icons = Object.keys(footerData.community);
+  const iconLinks = Object.values(footerData.community);
+
+  return (
+    <footer className="mt-16 grid grid-cols-4 border-t-2 px-32 py-20 text-white">
+      <Image
+        src="/assets/footer/logo.png"
+        alt=""
+        className="object-contain"
+        width={75}
+        height={75}
+      />
+      <div className="flex flex-col gap-y-3">
+        <h3 className="text-xl">acm at utd</h3>
+        <div className="flex flex-col">
+          {footerData.links.map((link, idx) => {
+            return (
+              <Link href={link.link} key={idx}>
+                {link.text}
+              </Link>
+            );
+          })}
+        </div>
+      </div>
+      <div className="flex flex-col">
+        <h3 className="text-xl">reach us at</h3>
+        <a href={`mailto:${footerData.contact}`}>{footerData.contact}</a>
+      </div>
+      <div>
+        <h3 className="text-xl">connect with us</h3>
+        <div className="grid grid-cols-3 w-fit gap-x-6 gap-y-3">
+          {icons.map((icon: string, idx: number) => {
+            return (
+              <a href={iconLinks[idx]} key={idx}>
+                <Image
+                  src={`/assets/home/icons/${icon}.png`}
+                  alt=""
+                  className="object-contain"
+                  width={25}
+                  height={25}
+                />
+              </a>
+            );
+          })}
+        </div>
+      </div>
+    </footer>
+  );
+}
