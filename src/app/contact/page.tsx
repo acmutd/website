e34fd9b6822c4d904086782471aa8707be42be90
Bhavesh Mandalapu <mandalapubhavesh@gmail.com>
2024-03-12 16:25:49 -0500

    Fix: Padding change

--- src/app/contact/page.tsx
+++ src/app/contact/page.tsx
@@ -3,7 +3,7 @@ import Form from '@/components/Contact/Form';
 
 export default function Contact() {
   return (
-    <div className="flex h-fit flex-col gap-y-56 bg-[url(/assets/apply/apply-bg.png)] bg-cover pt-24 text-white">
+    <div className="flex h-fit flex-col gap-y-56 bg-[url(/assets/apply/apply-bg.png)] bg-cover py-20 text-white">
       <main className="flex flex-col justify-between gap-x-8 gap-y-10 p-10 md:flex-row lg:flex-row">
         <Side />
         <Form />
