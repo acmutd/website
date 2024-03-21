cfb231f2439c6b9ae45e1a21c8874ff33395f52c
Ruben Olano <rubendavid2711@gmail.com>
2024-03-21 08:28:34 -0500

    Add embeded google calendar for events

--- src/components/Events/Calendar.tsx
+++ src/components/Events/Calendar.tsx
@@ -1,69 +1,12 @@
-'use client';
-
-import { useReducer } from 'react';
-import CalendarBody from './CalendarBody';
-import type { Event } from '../../../lib/types';
-
-const monthNames = [
-  'January',
-  'February',
-  'March',
-  'April',
-  'May',
-  'June',
-  'July',
-  'August',
-  'September',
-  'October',
-  'November',
-  'December',
-];
-
-type Action = { type: 'NEXT_MONTH' } | { type: 'PREV_MONTH' };
-type State = { currentYear: number; currentMonth: number; events: Event[] };
-
-function reducer(state: State, action: Action): State {
-  const newEvent: Event = {
-    date: new Date(state.currentYear, state.currentMonth, 27),
-    title: 'Projects Presentation Night',
-    id: '1',
-    location: '',
-  };
-  switch (action.type) {
-    case 'NEXT_MONTH':
-      const nextMonth = state.currentMonth + 1;
-      return {
-        currentMonth: nextMonth > 11 ? 0 : nextMonth,
-        currentYear: nextMonth > 11 ? state.currentYear + 1 : state.currentYear,
-        events: [newEvent],
-      };
-    case 'PREV_MONTH':
-      const prevMonth = state.currentMonth - 1;
-      return {
-        currentMonth: prevMonth < 0 ? 11 : prevMonth,
-        currentYear: prevMonth < 0 ? state.currentYear - 1 : state.currentYear,
-        events: [],
-      };
-  }
-}
-
 function Calendar() {
-  const [state, dispatch] = useReducer(reducer, {
-    currentYear: new Date().getFullYear(),
-    currentMonth: new Date().getMonth(),
-    events: [{ date: new Date(), title: 'Projects Presentation Night', id: '1', location: '' }],
-  });
-
   return (
     <div className="rounded-3xl border border-[#ffffff82] bg-gradient-to-tr from-[#ffffff1f] from-[3.07%] to-[#ffffff08] to-[96.39%] p-5 shadow-md backdrop-blur-xl">
-      <div className="mb-5 flex items-center justify-between text-white">
-        <button className="w-10" onClick={() => dispatch({ type: 'PREV_MONTH' })}>{`<`}</button>
-        <span className="w-32 text-center text-2xl font-semibold">
-          {monthNames[state.currentMonth]}
-        </span>
-        <button className="w-10" onClick={() => dispatch({ type: 'NEXT_MONTH' })}>{`>`}</button>
-      </div>
-      <CalendarBody month={state.currentMonth} year={state.currentYear} events={state.events} />
+      <iframe
+        src="https://calendar.google.com/calendar/embed?src=c_893d7ec01b0d651ddfadf46f6792b1d470abe97c6d9f33157a1f4be2d4420a51%40group.calendar.google.com&ctz=America%2FChicago"
+        title="ACM Calendar"
+        height={657}
+        width={540}
+      />
     </div>
   );
 }
