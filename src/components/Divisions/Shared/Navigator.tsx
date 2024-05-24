8c54f9e4b33523848091b9f78ef0ffd87ef2f063
Ruben Olano <rubendavid2711@gmail.com>
2024-05-23 19:43:02 -0500

    fix: change styling so that the navigator links don't take up the full width of the screen as well as some other styling changes

--- src/components/Divisions/Shared/Navigator.tsx
+++ src/components/Divisions/Shared/Navigator.tsx
@@ -1,5 +1,6 @@
 import Link from 'next/link';
 import { Division } from './divisionUtil';
+import React from 'react';
 
 const mentorLinks = ['directors', 'experience', 'workshops', 'faq'];
 const altLinks = ['directors', 'experience', 'winning projects', 'faq'];
@@ -28,15 +29,16 @@ export default function Navigator(props: Props) {
   if (props.division === 'education') routeLinks = LINK_DICT.education[props.sub];
   else routeLinks = LINK_DICT[props.division];
   return (
-    <div className="sticky right-5 top-1/3 z-50 flex h-full w-full flex-col space-y-5 pr-10 text-right">
+    <div className="sticky right-5 top-1/3 z-50 h-full pr-10 text-right">
       {routeLinks.map((link) => (
-        <Link
-          className="text-2xl font-semibold text-white transition-all duration-300 hover:text-[#F3F3F3]"
-          key={link}
-          href={`#${link.split(' ').join('-').toLowerCase()}`}
-        >
-          {link}
-        </Link>
+        <div className="pb-5" key={link}>
+          <Link
+            className="pb-5 text-2xl font-semibold text-white transition-all duration-300 hover:text-gray-300"
+            href={`#${link.split(' ').join('-').toLowerCase()}`}
+          >
+            {link}
+          </Link>
+        </div>
       ))}
     </div>
   );
