c4ce30c4cabc13eb580e9e80f2088a15e53c7f18
Ruben Olano <rubendavid2711@gmail.com>
2024-05-23 23:23:11 -0500

    feat: Add data for hackutd

---
+++ config/hack.config.ts
@@ -0,0 +1,189 @@
+import { type Officer } from './officers.config';
+
+export const hackOfficers: Officer[] = [
+  {
+    name: 'Purva Pawar',
+    position: 'HackUTD Director',
+    image: '/assets/officer/OfficerImage.png',
+  },
+  {
+    name: 'Salman Jaher',
+    position: 'HackUTD Director',
+    image: '/assets/officer/OfficerImage.png',
+  },
+  {
+    name: 'Joshua Das',
+    position: 'Experience Lead',
+    image: '/assets/officer/OfficerImage.png',
+  },
+  {
+    name: 'Evan Ngo',
+    position: 'Experience Officer',
+    image: '/assets/officer/OfficerImage.png',
+  },
+  {
+    name: 'Reiki Hingorani',
+    position: 'Experience Officer',
+    image: '/assets/officer/OfficerImage.png',
+  },
+  {
+    name: 'Alan Roybal',
+    position: 'Experience Officer',
+    image: '/assets/officer/OfficerImage.png',
+  },
+  {
+    name: 'Veer Shah',
+    position: 'Experience Officer',
+    image: '/assets/officer/OfficerImage.png',
+  },
+  {
+    name: 'Arjun Kumar',
+    position: 'Experience Officer',
+    image: '/assets/officer/OfficerImage.png',
+  },
+  {
+    name: 'Daniel Kim',
+    position: 'Experience Officer',
+    image: '/assets/officer/OfficerImage.png',
+  },
+  {
+    name: 'Adelaide Dunning',
+    position: 'Logistics Lead',
+    image: '/assets/officer/OfficerImage.png',
+  },
+  {
+    name: 'Anwita Gudapurri',
+    position: 'Logistics Coordinator',
+    image: '/assets/officer/OfficerImage.png',
+  },
+  {
+    name: 'Shreya Mekala',
+    position: 'Logistics Coordinator',
+    image: '/assets/officer/OfficerImage.png',
+  },
+  {
+    name: 'Roshni Reddhannan',
+    position: 'Logistics Coordinator',
+    image: '/assets/officer/OfficerImage.png',
+  },
+  {
+    name: 'Sahishnu Sagiraju',
+    position: 'Logistics Coordinator',
+    image: '/assets/officer/OfficerImage.png',
+  },
+  {
+    name: 'Alexander Tifase',
+    position: 'Logistics Coordinator',
+    image: '/assets/officer/OfficerImage.png',
+  },
+  {
+    name: 'Bhuvana Madiraju',
+    position: 'Industry Lead',
+    image: '/assets/officer/OfficerImage.png',
+  },
+  {
+    name: 'Sharun Naicker',
+    position: 'Industry Coordinator',
+    image: '/assets/officer/OfficerImage.png',
+  },
+  {
+    name: 'Sachi Hansalia',
+    position: 'Industry Coordinator',
+    image: '/assets/officer/OfficerImage.png',
+  },
+  {
+    name: 'Ridwan Amin',
+    position: 'Industry Coordinator',
+    image: '/assets/officer/OfficerImage.png',
+  },
+  {
+    name: 'Bryan Caraman',
+    position: 'Industry Coordinator',
+    image: '/assets/officer/OfficerImage.png',
+  },
+  {
+    name: 'John Luke',
+    position: 'Industry Coordinator',
+    image: '/assets/officer/OfficerImage.png',
+  },
+  {
+    name: 'Mike Nguyen',
+    position: 'Tech Lead',
+    image: '/assets/officer/OfficerImage.png',
+  },
+  {
+    name: 'Wei Seng',
+    position: 'Developer',
+    image: '/assets/officer/OfficerImage.png',
+  },
+  {
+    name: 'Rayyan Waris',
+    position: 'Developer',
+    image: '/assets/officer/OfficerImage.png',
+  },
+  {
+    name: 'Victoria Vynnychok',
+    position: 'Developer',
+    image: '/assets/officer/OfficerImage.png',
+  },
+  {
+    name: 'Anh Tu Do',
+    position: 'Developer',
+    image: '/assets/officer/OfficerImage.png',
+  },
+  {
+    name: 'Kelly Zhou',
+    position: 'Maketing Lead',
+    image: '/assets/officer/OfficerImage.png',
+  },
+  {
+    name: 'Mitchell Vu',
+    position: 'Video Editor',
+    image: '/assets/officer/OfficerImage.png',
+  },
+  {
+    name: 'Shaz Kumar',
+    position: 'Social Media',
+    image: '/assets/officer/OfficerImage.png',
+  },
+  {
+    name: 'Jordan Tan',
+    position: 'Designer',
+    image: '/assets/officer/OfficerImage.png',
+  },
+  {
+    name: 'Alyssa To',
+    position: 'Designer/UI/UX',
+    image: '/assets/officer/OfficerImage.png',
+  },
+  {
+    name: 'Ann Jayan',
+    position: 'Designer',
+    image: '/assets/officer/OfficerImage.png',
+  },
+  {
+    name: 'Sarah Jacob',
+    position: 'Designer',
+    image: '/assets/officer/OfficerImage.png',
+  },
+  {
+    name: 'Caleb Bae',
+    position: 'Outreach',
+    image: '/assets/officer/OfficerImage.png',
+  },
+  {
+    name: 'Andrew Goodland',
+    position: 'Finance',
+    image: '/assets/officer/OfficerImage.png',
+  },
+  {
+    name: 'Ayusha Timalsena',
+    position: 'Finance',
+    image: '/assets/officer/OfficerImage.png',
+  },
+  {
+    name: 'Michael :D',
+    position: 'Advisor',
+    image: '/assets/officer/OfficerImage.png',
+  },
+];
