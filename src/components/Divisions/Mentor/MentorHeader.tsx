c17e41b9692f9c1d3a363d6f1a1e85c9cc495296
DedsecKnight <mikenguyen7268@gmail.com>
2024-08-14 15:23:24 -0500

    feat: added header to mentor and tip program

---
+++ src/components/Divisions/Mentor/MentorHeader.tsx
@@ -0,0 +1,10 @@
+import DivisionHeader from '@/components/Divisions/Shared/DivisionHeader';
+
+export function MentorHeader() {
+  return (
+    <DivisionHeader division="education.mentor">
+      Get a chance to network, access resources like Udemy courses, and attend exclusive socials and
+      workshops! Apply to be a Mentor or a Mentee today!
+    </DivisionHeader>
+  );
+}
---
+++ src/components/Divisions/TIP/TIPHeader.tsx
@@ -0,0 +1,11 @@
+import DivisionHeader from '../Shared/DivisionHeader';
+
+export function TIPHeader() {
+  return (
+    <DivisionHeader division="education.tip">
+      Trying to land an internship? TIP is a 10-week-long program, focused on preparing you for
+      technical and behavioral interviews. The program will help build your intuition and practice
+      foundational data structures and algorithms present in most technical interviews.
+    </DivisionHeader>
+  );
+}
