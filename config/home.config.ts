0e1282dd7ee3753277ccac42a6182438298c1125
DedsecKnight <mikenguyen7268@gmail.com>
2024-08-14 14:37:45 -0500

    feat: changed design of contact page to match new mockup

--- config/home.config.ts
+++ config/home.config.ts
@@ -4,9 +4,9 @@ export const homePageData: HomePageData = {
   orgStatement:
     "We're a team of students with one goal: building a greater, more collaborative computing community at The University of Texas at Dallas.",
   community: {
-    linkedin: '/',
-    instagram: '/',
-    discord: '/',
+    linkedin: 'https://www.linkedin.com/company/acmutd',
+    instagram: 'https://www.instagram.com/acmutd/',
+    discord: 'https://discord.gg/ttB9HuaKsA',
     github: '/',
     youtube: '/',
   },
--- src/components/Contact/Form.tsx
+++ src/components/Contact/Form.tsx
@@ -1,34 +1,41 @@
 'use client';
 
+import Image from 'next/image';
+import Link from 'next/link';
+import { homePageData } from '../../../config/home.config';
+
 export default function Form() {
-  const handleSend = (e: React.MouseEvent<HTMLButtonElement>) => {
-    e.preventDefault();
-
-    console.log('Send email');
-  };
-
   return (
-    <form className="flex flex-col gap-y-8 rounded-2xl border-primary/50 bg-gray-300/10 p-12 text-primary">
-      <p className="text-lg lg:text-xl">
-        Have any questions, want to sponsor us, or just want to say hi? Send us a message
-      </p>
-      <div className="flex flex-col gap-y-1">
-        <label className="text-xl font-semibold lg:text-2xl">name</label>
-        <input type="text" className="h-10 rounded-xl text-black" required />
+    <div className="flex min-h-[700px] w-full flex-col gap-y-8 rounded-2xl border border-primary/50 bg-gray-300/10 p-12 text-primary md:w-1/2 md:rounded-l-2xl md:rounded-r-[0px] md:border-y md:border-l md:border-r-[0px]">
+      <p className="text-2xl font-bold lg:text-3xl">Connect with us!</p>
+      <div className="my-auto flex h-3/5 flex-col gap-y-8">
+        <Link href={homePageData.community.linkedin} passHref>
+          <div className="flex cursor-pointer items-center gap-x-3 gap-y-1">
+            <Image src="/assets/contact/linkedin.png" width={50} height={50} alt="LinkedIn" />
+            <label className="cursor-pointer text-2xl font-semibold lg:text-3xl">LinkedIn</label>
+          </div>
+        </Link>
+        <Link href={homePageData.community.instagram}>
+          <div className="flex cursor-pointer items-center gap-x-3 gap-y-1">
+            <Image src="/assets/contact/instagram.png" width={50} height={50} alt="Instagram" />
+            <label className="cursor-pointer text-2xl font-semibold lg:text-3xl">Instagram</label>
+          </div>
+        </Link>
+        <Link href={homePageData.community.discord}>
+          <div className="flex cursor-pointer items-center gap-x-3 gap-y-1">
+            <Image src="/assets/contact/discord.png" width={50} height={50} alt="Discord" />
+            <label className="cursor-pointer text-2xl font-semibold lg:text-3xl">Discord</label>
+          </div>
+        </Link>
+        <Link href="mailto:contact@acmutd.co">
+          <div className="flex cursor-pointer items-center gap-x-3 gap-y-1">
+            <Image src="/assets/contact/mail.png" width={50} height={50} alt="Mail" />
+            <label className="cursor-pointer text-2xl font-semibold lg:text-3xl">
+              contact@acmutd.co
+            </label>
+          </div>
+        </Link>
       </div>
-      <div className="flex flex-col gap-y-1">
-        <label className="text-xl font-semibold lg:text-2xl">email</label>
-        <input type="email" className="h-10 rounded-xl text-black" required />
-      </div>
-      <div className="flex flex-col gap-y-1">
-        <label className="text-xl font-semibold lg:text-2xl">message</label>
-        <textarea className="h-64 rounded-xl text-black" required />
-      </div>
-      <div className="flex justify-center">
-        <button className="w-fit rounded-xl border-2 px-8 py-2 text-2xl transition delay-150 duration-300 ease-in-out hover:-translate-y-1 hover:scale-110 hover:bg-acm-gradient" onClick={(event) => handleSend(event)}>
-          Send
-        </button>
-      </div>
-    </form>
+    </div>
   );
 }
