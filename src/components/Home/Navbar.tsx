ec0651a9fa88318773aa2fc80fc3cb459ab25481
DedsecKnight <mikenguyen7268@gmail.com>
2024-08-14 14:50:51 -0500

    refactor: changed occurrence of "contact" to "connect"

--- src/components/Home/Navbar.tsx
+++ src/components/Home/Navbar.tsx
@@ -61,7 +61,7 @@ export default function Navbar() {
                   />
                 </Link>
                 <div className="hidden sm:ml-6 sm:block ">
-                  <div className="flex space-x-10 items-center h-screen">
+                  <div className="flex h-screen items-center space-x-10">
                     {navigation.map((item) => (
                       <a
                         key={item.name}
