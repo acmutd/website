b70a1b00aa19770fa63df1610e1c1347e4f50b74
kumarshaswat <shaswat_kr@yahoo.com>
2024-08-14 21:17:36 -0500

    Update home.config.ts

--- config/home.config.ts
+++ config/home.config.ts
@@ -14,7 +14,7 @@ export const homePageData: HomePageData = {
     {
       description: 'join our mailing list to stay updated with our events and programs!',
       linkText: 'join',
-      link: '/',
+      link: 'https://cdn.forms-content.sg-form.com/22d851f4-5f47-11eb-9b58-e2c4feadfaf0',
     },
     {
       description: 'acm applications now open for fall 2024!',
