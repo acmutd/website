8f8e27c51c4be517d2ac344201e53a3acb51467b
DedsecKnight <mikenguyen7268@gmail.com>
2024-08-16 10:56:59 -0500

    chore: added link and fixed "past workshops" section

--- src/components/Events/PastWorkshops.tsx
+++ src/components/Events/PastWorkshops.tsx
@@ -8,21 +8,31 @@ const workshops: PastWorkshop[] = [
     description:
       'Learn how to make your very own personal website! Make a customizable portfolio website that showcases all your projects - and if you already have a personal website, learn how to integrate dark mode!',
     links: [
-      { type: 'github', link: '' },
-      { type: 'video', link: '' },
+      { type: 'github', link: 'https://github.com/acmutd/htf-education' },
+      { type: 'video', link: 'https://www.youtube.com/watch?v=X-XVUYu5unY' },
     ],
   },
   {
     title: 'Game Engine Development Workshop Series',
     description:
       'No matter your experience level, join the game engine development workshop series where we will be designing and writing our own game engine from scratch!',
-    links: [{ type: 'video', link: '' }],
+    links: [
+      {
+        type: 'video',
+        link: 'https://www.youtube.com/watch?v=88jguNWiD8k&list=PL5MSuy7M7Cr7-OoR00y9cLt3V4lDUrlgf&pp=iAQB',
+      },
+    ],
   },
   {
     title: 'CS Essentials Workshop Series',
     description: `Do you want to learn necessary programming skills which are
 not taught in class? Get a head start in your programming career by taking part in ACM's CS Essentials Bootcamp!`,
-    links: [{ link: '', type: 'video' }],
+    links: [
+      {
+        link: 'https://www.youtube.com/watch?v=rP9d2OvxXR0&list=PL5MSuy7M7Cr7QGSuZa4f3H6VeIc9-DQ_O&pp=iAQB',
+        type: 'video',
+      },
+    ],
   },
 ];
 
