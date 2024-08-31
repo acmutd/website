01c7ce87160a036bac7b7e269e0493a626af407f
Ari S <ari@aris-mbp.lan>
2024-08-31 14:13:40 -0500

    added md:block

--- src/app/officers/page.tsx
+++ src/app/officers/page.tsx
@@ -5,7 +5,7 @@ import Image from 'next/image';
 function Apply() {
   return (
     <div className="relative h-full w-full bg-[url(/assets/officer/officer-bg.png)] bg-cover py-20">
-      <div className="absolute right-20 top-28 hidden h-[105rem] w-0 flex-shrink-0 text-center text-[0.625rem] font-bold leading-[99.99%] tracking-[-0.01875rem] text-[#8A8A8A]">
+      <div className="absolute right-20 top-28 hidden h-[105rem] w-0 flex-shrink-0 text-center text-[0.625rem] font-bold leading-[99.99%] tracking-[-0.01875rem] text-[#8A8A8A] md:block">
         <p>
           0 1 0 0 0 0 0 1 0 1 0 0 0 0 1 1 0 1 0 0 1 1 0 1 0 0 1 0 0 0 0 0 0 1 0 1 0 0 0 0 0 1 0 1 0
           0 1 0 0 1 0 0 1 1 1 1 0 1 0 0 1 0 1 0 0 1 0 0 0 1 0 1 0 1 0 0 0 0 1 1 0 1 0 1 0 1 0 0 0 1
