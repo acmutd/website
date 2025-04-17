24edbf3959ad000da118583aaa505e9b239da4e8
Bhavesh Mandalapu <mandalapubhavesh@gmail.com>
2025-04-17 13:21:13 -0500

    Increase Apply page card size

--- src/components/Apply/ApplyProgramCard.tsx
+++ src/components/Apply/ApplyProgramCard.tsx
@@ -11,10 +11,16 @@ interface ApplyProgramCardProps {
 function ApplyProgramCard({ title, image, link }: ApplyProgramCardProps) {
   return (
     <Link href={link} className="relative flex w-full items-center justify-center">
-      <h1 className="absolute left-1/2 top-1/2 -translate-x-1/2 -translate-y-1/2 transform text-center text-xl md:text-4xl font-bold text-primary xl:text-5xl">
+      <h1 className="absolute left-1/2 top-1/2 -translate-x-1/2 -translate-y-1/2 transform text-center text-xl font-bold text-primary md:text-4xl xl:text-5xl">
         {title}
       </h1>
-      <Image src={image} alt={title} width={500} height={500} className="w-full rounded-3xl" />
+      <Image
+        src={image}
+        alt={title}
+        width={500}
+        height={500}
+        className="aspect-[6/3] w-full rounded-3xl object-cover"
+      />
     </Link>
   );
 }
