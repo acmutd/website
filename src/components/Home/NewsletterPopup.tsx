560f6f24ac885e96a7941b193f7f06cccb24c719
Jeydin21 <jeydin@j21.dev>
2025-06-27 18:18:46 -0500

    fix: Re-add old newsletter link

--- src/components/Home/NewsletterPopup.tsx
+++ src/components/Home/NewsletterPopup.tsx
@@ -68,6 +68,7 @@ export function NewsletterPopup() {
         <div className="flex justify-end">
           <Button
             href="/newsletter"
+            // Old link: https://cdn.forms-content.sg-form.com/22d851f4-5f47-11eb-9b58-e2c4feadfaf0
             text="join"
             bgStyle="acm"
           />
