3a1d5a60955143cb569db2025d7ab2753684589e
Jeydin21 <jeydin@j21.dev>
2025-08-16 19:55:26 -0500

    style(apply page header): Move to center and make all corners rounded

--- src/components/Apply/ApplyHeader.tsx
+++ src/components/Apply/ApplyHeader.tsx
@@ -3,8 +3,8 @@ import React from 'react';
 
 function ApplyHeader() {
   return (
-    <div className="flex min-h-[600px] w-full justify-end pl-10 md:min-h-[470px]">
-      <div className="relative mb-6 w-full overflow-hidden rounded-bl-3xl rounded-tl-3xl border border-primary/50 bg-apply-gradient shadow-2xl">
+    <div className="flex min-h-[600px] w-full justify-end px-10 md:min-h-[470px]">
+      <div className="relative mb-6 w-full overflow-hidden rounded-3xl border border-primary/50 bg-apply-gradient shadow-2xl">
         <div className="absolute left-0 top-0 flex h-full w-full flex-col items-center justify-center p-6 md:flex-row md:space-x-10">
           <div className="w-full max-w-md lg:max-w-xl">
             <h1 className="mb-10 text-3xl font-bold text-primary xl:text-5xl">apply</h1>
