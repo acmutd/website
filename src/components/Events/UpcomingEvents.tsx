c69724514718e0edc91062cb51f57dc562af33ad
Jeydin21 <JeyPham21@gmail.com>
2025-08-26 14:43:07 -0500

    refactor: Add safe whitespace character (%20) to replace the actual whitespaces in the calendar URL

--- src/components/Events/UpcomingEvents.tsx
+++ src/components/Events/UpcomingEvents.tsx
@@ -67,7 +67,7 @@ export default function UpcomingEvents() {
     const endDate = new Date(event.start.getTime() + 60 * 60 * 1000);
     const end = formatDateTime(endDate);
     const details = [event.description || '', event.location ? `Location: ${event.location}` : ''].filter(Boolean).join('%0A');
-    return `https://www.google.com/calendar/render?action=TEMPLATE&text=ACM UTD: ${encodeURIComponent(event.title)}&dates=${start}/${end}&details=${encodeURIComponent(details)}${event.location ? `&location=${encodeURIComponent(event.location)}` : ''}`;
+    return `https://www.google.com/calendar/render?action=TEMPLATE&text=ACM%20UTD:%20${encodeURIComponent(event.title)}&dates=${start}/${end}&details=${encodeURIComponent(details)}${event.location ? `&location=${encodeURIComponent(event.location)}` : ''}`;
   };
 
   return (
