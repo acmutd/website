858882c4767da0db1608ee48c18804f859b6ab2d
Ruben Olano <rubendavid2711@gmail.com>
2025-09-17 19:41:46 -0500

    fix broken buttons after external link change

--- src/components/Button.tsx
+++ src/components/Button.tsx
@@ -21,7 +21,7 @@ export function Button({
 }) {
   return (
     <Link
-      href={external ? href : `/${href}`}
+      href={external ? href : `${href}`}
       target={external ? '_blank' : '_self'}
       className={`content z-20 flex ${height} items-center justify-center rounded-lg border border-primary/50 bg-gray-300/10 bg-${bgStyle}-gradient px-3 sm:px-4 py-2 font-bold transition delay-75 duration-200 ease-in-out hover:scale-105 sm:hover:scale-110 ${width} ${textStyles} ${addtitionalStyles}`}
     >
