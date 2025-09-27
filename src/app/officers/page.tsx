dc435c6b25e89cf61182d12cf98ff41451a71cda
Ruben Olano <rubendavid2711@gmail.com>
2025-09-27 08:29:42 -0500

    circle gone and added peechi also moved components

--- src/app/officers/page.tsx
+++ src/app/officers/page.tsx
@@ -5,19 +5,21 @@ import { divisions, allDivisions, titleMap } from '@/components/Officers/constan
 
 function Officers() {
   return (
-    <div className="min-h-screen w-full relative">
+    <div className="relative min-h-screen w-full">
       <div className="fixed inset-0 bg-[url(/assets/officer/officer-bg.png)] bg-cover bg-center bg-no-repeat" />
 
       <div className="relative z-10">
         <OfficerHeader />
 
-        <div className="container mx-auto px-4 py-8 lg:px-8">
-          <OfficerClient
-            divisions={divisions}
-            allDivisions={allDivisions}
-            titleMap={titleMap}
-            divisionOfficerMap={divisionOfficerMap}
-          />
+        <div className="relative">
+          <div className="container relative z-10 mx-auto px-4 py-12 lg:px-8 lg:py-16">
+            <OfficerClient
+              divisions={divisions}
+              allDivisions={allDivisions}
+              titleMap={titleMap}
+              divisionOfficerMap={divisionOfficerMap}
+            />
+          </div>
         </div>
       </div>
     </div>
