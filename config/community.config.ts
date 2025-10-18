aafecc5f0d55ae64eae27d88d2c5f1f74b1f39a3
Jeydin21 <jeydin@j21.dev>
2025-10-18 02:21:30 -0500

    refactor: Fix community config file to separate Outreach and Campus

--- config/community.config.ts
+++ config/community.config.ts
@@ -2,6 +2,8 @@ import { Question, Testimony } from '../lib/types';
 import { CarouselImage, Director } from './divisions.config';
 import { type Officer } from './officers.config';
 
+type SubDivision = 'campus' | 'outreach';
+
 export const communityDirectors: Director[] = [
   {
     image: '/assets/officer/ShannonCarter.jpg',
@@ -93,30 +95,90 @@ export const communityOfficers: Officer[] = [
   },
 ];
 
-export const communityOutreachCarouselImages: CarouselImage[] = [
-  {
-    imageLink: '/assets/community/notes_floats.png',
-    title: 'Notes and Floats',
-    date: new Date(2025, 1, 11),
-  },
-  {
-    date: new Date(2025, 0,2),
-    imageLink: '/assets/community/group_photo.jpg',
-    title: 'Community'
-  }
-]
-export const communityOutreachTestimony: Testimony[] = [
-  {
-    quote: 'Being a part of the Events team has been such an exciting adventure. There’s nothing more fulfilling than the ability to see the tangible impacts created as a result of your efforts.',
-    name: 'Ayomide Tapere',
-  },
-  {
-    quote: "Not only have I gotten the chance to meet so many wonderful people, but I’ve also gotten the opportunity to see all of our efforts have a positive impact on our community."
-  },
-  {
-    quote: "My favorite thing about events is that we get to help bring our organization and community together in a fun and exciting way that’s taught me so much about working on a team."
-  }
-]
+export const communityImages: {
+  campus: CarouselImage[];
+  outreach: CarouselImage[];
+} = {
+  campus: [
+    {
+      imageLink: '/assets/community/campus/notes_and_floats.png',
+      title: 'Notes and Floats',
+      date: new Date(2025, 1, 11),
+    },
+    {
+      date: new Date(2025, 0, 2),
+      imageLink: '/assets/community/campus/group_photo.jpg',
+      title: 'Community'
+    },
+    {
+      imageLink: '/assets/community/campus/twilight_tunes.jpg',
+      title: 'Campus Event 1',
+      date: new Date(2025, 10, 5),
+    }
+  ],
+  outreach: [
+    {
+      imageLink: '/assets/community/outreach/group_photo.jpg',
+      title: 'Outreach (temp)',
+      date: new Date(2025, 1, 11),
+    },
+  ]
+}
+
+export const communityTestimony: Record<SubDivision, Array<Testimony>> = {
+  campus: [
+    {
+      quote: 'Being a part of the Events team has been such an exciting adventure. There’s nothing more fulfilling than the ability to see the tangible impacts created as a result of your efforts.',
+      name: 'Ayomide Tapere',
+    },
+    {
+      quote: "Not only have I gotten the chance to meet so many wonderful people, but I’ve also gotten the opportunity to see all of our efforts have a positive impact on our community."
+    },
+    {
+      quote: "My favorite thing about events is that we get to help bring our organization and community together in a fun and exciting way that’s taught me so much about working on a team."
+    }
+  ],
+  outreach: [
+    {
+      quote: 'Being a part of the Events team has been such an exciting adventure. There’s nothing more fulfilling than the ability to see the tangible impacts created as a result of your efforts.',
+      name: 'Ayomide Tapere',
+    },
+    {
+      quote: "Not only have I gotten the chance to meet so many wonderful people, but I’ve also gotten the opportunity to see all of our efforts have a positive impact on our community."
+    },
+    {
+      quote: "My favorite thing about events is that we get to help bring our organization and community together in a fun and exciting way that’s taught me so much about working on a team."
+    }
+  ],
+}
+
+export const communityFAQ: Record<SubDivision, Question[]> = {
+  campus: [
+    {
+      question: 'What is the application process like?',
+      answer: 'The first step is to apply! Applications will include a series of questions to get to know you a little bit more and give us some insight about what you’d like to bring to ACM Campus! After these applications, we’ll select a few candidates to interview before deciding _____. But remember! Events and Archives applications open at different times so make sure to apply to the team you’d like to be on!',
+    },
+    {
+      question: 'How is ACM Campus structured?',
+      answer: 'ACM Campus is made of two teams: Events and Archives. Events handles all of the social events like Twilight Tunes and Peechi Games while Archives is a team of designers and writers, in charge of creating a magazine every semester that covers all of the behind the scenes of ACM for alumni and sponsors.',
+    },
+    {
+      question: 'Do I need to have prior experience in event planning to join ACM Events?',
+      answer: 'Nope! What we’re really looking for are students',
+    },
+    {
+      question: 'What are some events that the Events team has hosted in the past?',
+      answer: 'We get to',
+    }
+  ],
+  outreach: [
+    {
+      question: 'What is ACM Community Outreach?',
+      answer: 'ACM Community Outreach is a division of ACM that focuses on increasing STEM engagement in K-12 students, particularly in underserved schools. We organize various outreach events and programs to inspire and educate the next generation of technologists.'
+    }
+  ]
+};
+
 export const communityOutreachFAQ: Question[] = [
   {
     question: 'Where can I find upcoming events?',
@@ -131,13 +193,3 @@ export const communityOutreachFAQ: Question[] = [
     answer: 'At the moment, we’re currently not open for applications, but make sure to stay in touch by submitting any event ideas and be on the lookout for recruitment opportunities and upcoming events.'
   }
 ]
-
-// Campus-specific (currently reuse outreach data as sensible defaults)
-export const communityCampusCarouselImages: CarouselImage[] = communityOutreachCarouselImages;
-export const communityCampusTestimony: Testimony[] = communityOutreachTestimony;
-export const communityCampusFAQ: Question[] = communityOutreachFAQ;
-
-// Backwards-compatible aggregate exports used by older code paths
-export const communitCarouselImages = communityOutreachCarouselImages;
-export const communityTestimony = communityOutreachTestimony;
-export const communityFAQ = communityOutreachFAQ;
