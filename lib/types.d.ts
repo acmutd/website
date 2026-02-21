87343c9b6a40f58b40844160fc094dde10b418c8
Jeydin21 <jeydin@j21.dev>
2026-02-20 23:18:55 -0600

    fix: Add image module declarations for various formats

--- lib/types.d.ts
+++ lib/types.d.ts
@@ -116,3 +116,23 @@ export type DevProjects = {
   links: ProjectLink[];
   image: string;
 };
+
+declare module '*.png' {
+  const src: string;
+  export default src;
+}
+
+declare module '*.jpg' {
+  const src: string;
+  export default src;
+}
+
+declare module '*.jpeg' {
+  const src: string;
+  export default src;
+}
+
+declare module '*.webp' {
+  const src: string;
+  export default src;
+}
